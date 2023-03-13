ab53b5006327f95616a0b8050d7ef29b167f356e
flover.am-powershall <211250152@smail.nju.edu.cn>
2023-03-13 21:54:37 +0800

    初始化文件结构

---
+++ Include/test.h
@@ -0,0 +1,19 @@
+//
+// Created by 18466 on 2023/3/13.
+//
+
+#ifndef HUAWEISOFTWARECOMPETITION_TEST_H
+#define HUAWEISOFTWARECOMPETITION_TEST_H
+
+
+class test {
+private:
+    int value;
+public:
+    void setValue(int);
+
+    int getValue();
+};
+
+
+#endif //HUAWEISOFTWARECOMPETITION_TEST_H
---
+++ Source/test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 18466 on 2023/3/13.
+//
+
+#include "../Include/test.h"
+
+void test::setValue(int value) {
+    this->value = value;
+}
+
+int test::getValue() {
+    return value;
+}
