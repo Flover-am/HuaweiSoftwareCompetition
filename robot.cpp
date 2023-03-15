e65fca7226da733053136e31a829fb175c0322b2
Gloanming <211250153@smail.nju.edu.cn>
2023-03-15 18:46:51 +0800

    first

---
+++ robot.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 24873 on 2023/3/14.
+//
+
+#include "robot.h"
---
+++ robot.h
@@ -0,0 +1,19 @@
+//
+// Created by 24873 on 2023/3/14.
+//
+
+#ifndef HUAWEISOFTWARECOMPETITION_ROBOT_H
+#define HUAWEISOFTWARECOMPETITION_ROBOT_H
+
+
+class robot {
+public:
+    float palstance;
+    float lineSpeed_X;
+    float lineSpeed_Y;
+
+
+};
+
+
+#endif //HUAWEISOFTWARECOMPETITION_ROBOT_H
